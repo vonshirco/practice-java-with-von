838bcff024409d159019b3a123ea869edb0936a6
Von <vonshirco.vs@gmail.com>
2022-04-02 19:40:46 +0300

    Add files via upload

---
+++ 2.0 variablesandoperators/AreaTriangle.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udsm.variablesandoperators;
+
+/**
+ *
+ * @author von
+ */
+public class AreaTriangle {
+    public static void main(String[] args) {
+        System.out.println("The are of the triangle is " + 0.5*16*27 + " squared units");
+    }
+}
