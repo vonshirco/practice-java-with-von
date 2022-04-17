dd2cffeafc20ee6a251bb7c11b80612e81bf4bcf
Von <vonshirco.vs@gmail.com>
2022-04-17 15:51:35 +0300

    Add files via upload

---
+++ 04. datatypesAndConditions/javaBooleans.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udsm.datatypesandconditions;
+
+/**
+ *
+ * @author von
+ */
+public class javaBooleans {
+    public static void main(String[] args){
+        //Declaring and Assigning boleean variables
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        //Output
+        System.out.println(isJavaFun); //Outputs true
+        System.out.println(isFishTasty); //Outputs false
+    }
+    
+}
