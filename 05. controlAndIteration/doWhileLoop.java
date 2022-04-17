600e67cfc7a978c1a7f49dbc5ea56af6b30c2370
Von <vonshirco.vs@gmail.com>
2022-04-18 02:00:16 +0300

    Add files via upload

---
+++ 05. controlAndIteration/doWhileLoop.java
@@ -0,0 +1,13 @@
+
+package udsm.controlanditeration;
+
+public class doWhileLoop {
+    public static void main(String[] args){
+        int i = 0; //Initialization
+        do
+        {
+            System.out.println("i is : " + i);
+            i++; //Shift/Change
+        }while(i<4); //Condition
+    }
+}
