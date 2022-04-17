6e899a408af1bfe3b8b671fbd840d8399acb2b41
Von <vonshirco.vs@gmail.com>
2022-04-17 15:48:04 +0300

    Add files via upload

---
+++ 04. datatypesAndConditions/comments.java
@@ -0,0 +1,18 @@
+/*
+This is multiple lines comment
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udsm.datatypesandconditions;
+
+/**
+ *
+ * @author von
+ */
+public class comments {
+    public static void main(String[] args){
+        //This is Single line comment
+        System.out.println("Hello, Java Developers");
+    }
+}
