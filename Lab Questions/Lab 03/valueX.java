bda42f5ee9ece55721db3254324b824163d2b0c2
Von <vonshirco.vs@gmail.com>
2022-04-23 01:08:52 +0300

    Add files via upload

---
+++ Lab Questions/Lab 03/valueX.java
@@ -0,0 +1,22 @@
+
+package udsm.classesandobjects;
+ //Class
+class valueClass{
+    //Defining Method
+    public double equation(){
+        return (46/6) + (5%7);
+    }
+}
+
+public class valueX {
+    public static void main(String[] args){
+        //Creaning an Object
+        valueClass myValue = new valueClass();
+        
+        //Calling method
+        double x = myValue.equation();
+        
+        //Displaying the output/Value of x
+        System.out.println("The value of x is "+x);
+    }
+}
