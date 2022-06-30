52ff8c85426745d9dbef7f00ad45cb65fdf89a38
Von <vonshirco.vs@gmail.com>
2022-06-30 23:47:21 +0300

    Delete 2.0 variablesandoperators directory

--- 2.0 variablesandoperators/AreaTriangle.java
+++
@@ -1,7 +0,0 @@
-package udsm.variablesandoperators;
-
-public class AreaTriangle {
-    public static void main(String[] args) {
-        System.out.println("The are of the triangle is " + 0.5*16*27 + " squared units");
-    }
-}
